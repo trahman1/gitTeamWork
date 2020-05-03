23bb018771e6010c9ac9634d2fd00d2d5505453e
Tanjina Rahman <tanjinarahman@Tanjinas-MBP.home>
2020-05-03 13:31:33 -0400

    pizza details updated with couple of variables , constructor and getter setter

--- src/pizza/PizzaDetails.java
+++ src/pizza/PizzaDetails.java
@@ -2,7 +2,56 @@ package pizza;
 
 public class PizzaDetails {
 
+ public int pizzaName;
+ private String pizzaInfo;
 
+ private int ratings;
+ public int price;
+
+
+
+    public String getPizzaInfo() {
+        return pizzaInfo;
+    }
+
+    public void setPizzaInfo(String pizzaInfo) {
+        this.pizzaInfo = pizzaInfo;
+    }
+
+    public int getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(int ratings) {
+        this.ratings = ratings;
+    }
+
+    public PizzaDetails(int pizzaName, String pizzaInfo) {
+        this.pizzaName = pizzaName;
+        this.pizzaInfo = pizzaInfo;
+    }
+
+    public PizzaDetails(int price) {
+        this.price = price;
+    }
+
+    public PizzaDetails(int ratings, int price) {
+        this.ratings = ratings;
+        this.price = price;
+    }
+
+    public void pizzaName(){
+        System.out.println("Deluxe pizza");
+    }
+
+    public void taste(){
+        System.out.println("taste is good");
+    }
+
+
+    public void spicyPizza(){
+        System.out.println("i like spicy pizza");
+    }
 
 
 
