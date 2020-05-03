485efdca50c2470a6063b8587e247b0a988c7d9e
mhshahib <mh.shahib@gmail.com>
2020-05-03 13:00:32 -0400

    variable added and constructor creater and getter setter and display method created

--- src/burger/BurgerDetails.java
+++ src/burger/BurgerDetails.java
@@ -1,4 +1,57 @@
 package burger;
 
 public class BurgerDetails {
+    private String nameOfBurger;
+    private String nameTopping;
+    private double priceOfBurger;
+
+
+    public BurgerDetails() {
+    }
+
+    public BurgerDetails(String nameOfBurger) {
+        this.nameOfBurger = nameOfBurger;
+    }
+
+    public BurgerDetails(String nameOfBurger, String nameTopping) {
+        this.nameOfBurger = nameOfBurger;
+        this.nameTopping = nameTopping;
+    }
+
+    public BurgerDetails(String nameOfBurger, String nameTopping, double priceOfBurger) {
+        this.nameOfBurger = nameOfBurger;
+        this.nameTopping = nameTopping;
+        this.priceOfBurger = priceOfBurger;
+    }
+
+    public String getNameOfBurger() {
+        return nameOfBurger;
+    }
+
+    public void setNameOfBurger(String nameOfBurger) {
+        this.nameOfBurger = nameOfBurger;
+    }
+
+    public String getNameTopping() {
+        return nameTopping;
+    }
+
+    public void setNameTopping(String nameTopping) {
+        this.nameTopping = nameTopping;
+    }
+
+    public double getPriceOfBurger() {
+        return priceOfBurger;
+    }
+
+    public void setPriceOfBurger(double priceOfBurger) {
+        this.priceOfBurger = priceOfBurger;
+    }
+
+    public static void burgerInfo(){
+        System.out.println("Burger Info");
+    }
+
+
+
 }
